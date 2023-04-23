de301dde825e5b7476b424dd0407e8eed332688f
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-23 13:26:50 -0400

    update cards

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Intro1 from "./components/Intro1";
 import Intro2 from "./components/Intro2";
 import Intro3 from "./components/Intro3";
 import Footer from "./components/Footer";
+import Card from "./components/Cards";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
       <Intro2 />
       <Intro3 />
       <Footer />
+      <Card h2="First cards h2" h3="First Card h3" />
+      <Card h2="Second cards h2" h3="Second Card h3" />
+      <Card h2="Third cards h2" h3="Third Card h3" />
     </div>
   );
 }
